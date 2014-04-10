639d5bef900001813f60fe526ca037b58c144dc6
Raphael S. Carvalho <raphaelsc@cloudius-systems.com>
2014-04-10 17:37:48 -0300

    Port random device from FreeBSD

--- bsd/sys/sys/module.h
+++ bsd/sys/sys/module.h
@@ -1,3 +1,16 @@
 #ifndef _OSV_BSD_MODULE_H
 #define _OSV_BSD_MODULE_H
+
+typedef struct module *module_t;
+typedef int (*modeventhand_t)(module_t, int /* modeventtype_t */, void *);
+
+/*
+ * Struct for registering modules statically via SYSINIT.
+ */
+typedef struct moduledata {
+	const char	*name;		/* module name */
+	modeventhand_t  evhand;		/* event handler */
+	void		*priv;		/* extra data */
+} moduledata_t;
+
 #endif
