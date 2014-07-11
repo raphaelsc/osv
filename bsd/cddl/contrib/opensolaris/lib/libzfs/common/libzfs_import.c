f62da4cdb00f6d9cadfa458fd4c4ae60e9131c52
Prasad Joshi <prasadjoshi.linux@gmail.com>
2014-07-12 01:41:38 +0530

    libzfs: skip console device from zpool pool lookup
    Signed-off-by: Prasad Joshi <prasadjoshi.linux@gmail.com>

--- bsd/cddl/contrib/opensolaris/lib/libzfs/common/libzfs_import.c
+++ bsd/cddl/contrib/opensolaris/lib/libzfs/common/libzfs_import.c
@@ -1240,6 +1240,16 @@ zpool_find_import_impl(libzfs_handle_t *hdl, importargs_t *iarg)
 			    (name[1] == 0 || (name[1] == '.' && name[2] == 0)))
 				continue;
 
+#ifdef __OSV__
+			/*
+			* we are about to read from each device - reading from
+			* console would block indefinitely
+			*/
+			if (strcmp(name, "console") == 0) {
+				continue;
+			}
+#endif
+
 			slice = zfs_alloc(hdl, sizeof (rdsk_node_t));
 			slice->rn_name = zfs_strdup(hdl, name);
 			slice->rn_avl = &slice_cache;
